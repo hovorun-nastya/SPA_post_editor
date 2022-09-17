77367fc0089e1ce243e5ab8959894654ce154e3e
Anastasia <94125319+hovorun-nastya@users.noreply.github.com>
2022-09-17 22:00:02 +0200

    Connect comments with firebase

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,28 +1,44 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import {useParams, Route, Link, useRouteMatch} from "react-router-dom";
 import Comments from "../comments/Comments";
 import HighlightedQuote from "../quotes/HighlightedQuote";
 import NotFound from "./NotFound";
+import useHttp from "../../hooks/use-http";
+import {getSingleQuote} from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
-const DUMMY_QUOTES = [
-    {id: 'q1', author: 'Nastya', text: 'Learning React is fun !'},
-    {id: 'q2', author: 'Anastasiia', text: 'Learning React is great !'},
-];
 
 const QuoteDetail = () => {
     const match = useRouteMatch();
     const params = useParams();
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
-    if (!quote)
+    const {quoteId} = params;
+
+    const {sendRequest, status, data: loadedQuote, error} = useHttp(getSingleQuote, true)
+
+    useEffect(() => {
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId])
+
+    if (status === 'pending'){
+        return <div className='centered'>
+            <LoadingSpinner/>
+        </div>
+    }
+
+    if(error){
+        return <p className='centered'>{error}</p>
+    }
+    if (!loadedQuote.text)
         return (
             <NotFound/>
         )
+
     return (
         <Fragment>
-            <HighlightedQuote text={quote.text} author={quote.author}/>
+            <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author}/>
             <Route path={match.path} exact>
                 <div className="centered">
-                    <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>
+                    <Link className='btn--flat' to={`${match.url}/comments`}>
                         Load Comments
                     </Link>
                 </div>
--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -17,12 +17,18 @@ const sortQuotes = (quotes, ascending) => {
 const QuoteList = (props) => {
     const history = useHistory()
     const location = useLocation()
+
     const queryParams = new URLSearchParams(location.search);
     const isSortingAscending = queryParams.get('sort') === 'asc';
     const sortedQuotes = sortQuotes(props.quotes, isSortingAscending)
+
     const changeSortingHandler = () => {
-        history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+        history.push({
+            pathname: location.pathname,
+            search: `?sort=${(isSortingAscending ? 'desc' : 'asc')}`
+        })
     }
+
     return (
         <Fragment>
             <div className={classes.sorting}>
